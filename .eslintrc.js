66f4725ea030ca4ad6da953bb7174d07b77bd83d
Sergei Polishsuk <sepolish@student.jyu.fi>
2018-01-31 15:47:03 +0200

    demo1 valmis

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    }
+};
