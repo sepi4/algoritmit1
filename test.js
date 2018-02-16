b1efed5bf07973a9b8310f8c941a516750c4613e
Sergei Polishsuk <sepolish@student.jyu.fi>
2018-02-16 04:39:23 +0200

    sain demo3 ohjelmointi tehtavan valmiiksi

--- test.js
+++ test.js
@@ -22,8 +22,70 @@ Solmu.prototype.insert = function (key) {
   return this;
 };
 
-let puu = new Solmu(2);
-puu.insert(3);
-puu.insert(33);
-puu.insert(1);
+Solmu.prototype.esi = function(callback) {
+  if (this.key) {
+    callback(this.key);
+  }
+  if (this.left) {
+    this.left.esi(callback);
+  }
+  if (this.right) {
+    this.right.esi(callback);
+  }
+};
+
+Solmu.prototype.sisa = function(callback) {
+  if (this.left) {
+    this.left.esi(callback);
+  }
+  if (this.key) {
+    callback(this.key);
+  }
+  if (this.right) {
+    this.right.esi(callback);
+  }
+};
+
+Solmu.prototype.jalki = function(callback) {
+  if (this.left) {
+    this.left.esi(callback);
+  }
+  if (this.right) {
+    this.right.esi(callback);
+  }
+  if (this.key) {
+    callback(this.key);
+  }
+};
+
+function tulosta (key) {
+  console.log(key);
+}
+
+let puu = new Solmu(6);
 puu.insert(2);
+puu.insert(4);
+puu.insert(3);
+puu.insert(5);
+puu.insert(0);
+puu.insert(1);
+puu.insert(8);
+puu.insert(7);
+puu.insert(9);
+puu.insert(10);
+
+
+
+
+
+console.log('esijarjestyksessa tulostus:')
+puu.esi(tulosta);
+console.log('-----------------');
+
+console.log('sisajarjestyksessa tulostus:')
+puu.sisa(tulosta);
+console.log('-----------------');
+
+console.log('jalkijarjestyksessa tulostus:')
+puu.jalki(tulosta);
+console.log('-----------------');
